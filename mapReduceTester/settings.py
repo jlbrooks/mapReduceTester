07328c21192258ca0a344c9956282ed8ba5819c7
Jacob Brooks <jacob@chrisbrooks.org>
2016-01-05 19:47:32 -0500

    Running arbitrary submitted jar files

--- mapReduceTester/settings.py
+++ mapReduceTester/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'testServer',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+FILE_UPLOAD_HANDLERS = ["django.core.files.uploadhandler.TemporaryFileUploadHandler"]
--- testServer/views.py
+++ testServer/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import subprocess
+import os
 
-# Create your views here.
+def index(request):
+  return render(request, 'index.html')
+
+def submit(request):
+  if request.method == 'GET':
+    return redirect(index)
+
+  f = request.FILES.get('java_file')
+
+  print(f.temporary_file_path())
+
+  p1 = subprocess.Popen("java -jar %s" % f.temporary_file_path(), shell=True, stdout=subprocess.PIPE)
+  message = p1.stdout.read()
+
+  context = {
+    'message': message
+  }
+
+  return render(request, 'index.html', context)
