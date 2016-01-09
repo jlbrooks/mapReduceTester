007450bb8495488d1aca447c8159bd206d9d8dc5
Jacob Brooks <jacob@chrisbrooks.org>
2016-01-09 00:45:39 -0500

    Java dir pathing

--- testServer/views.py
+++ testServer/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render, redirect
+from django.conf import settings
 import subprocess
 import os
+import uuid
+
+def setup_javadir(f):
+  # Create the folder if it doesn't exist.
+  try:
+    os.mkdir(settings.JAVA_DIR)
+  except:
+    pass
+
+  # Create a unique folder name for this java run
+  folder = os.path.join(settings.JAVA_DIR, str(uuid.uuid1()))
+  filename = os.path.join(folder, f.name)
+  print(filename)
+
 
 def index(request):
   return render(request, 'index.html')
@@ -11,7 +26,7 @@ def submit(request):
 
   f = request.FILES.get('java_file')
 
-  print(f.temporary_file_path())
+  setup_javadir(f)
 
   p1 = subprocess.Popen("java -jar %s" % f.temporary_file_path(), shell=True, stdout=subprocess.PIPE)
   message = p1.stdout.read()
